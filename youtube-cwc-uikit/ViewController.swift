53ca6835cbc67ed0629cce5c96bfff34678f6970
Leonardo de Oliveira <leeonardo.deo@gmail.com>
2023-06-28 22:25:14 -0300

    Remove storyboard

---
+++ youtube-cwc-uikit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  youtube-cwc-uikit
+//
+//  Created by Leonardo de Oliveira on 28/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
