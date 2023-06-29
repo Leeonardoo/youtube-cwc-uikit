13430ec9900595cd3c9a573dfe17701ef8e0da11
Leonardo de Oliveira <leeonardo.deo@gmail.com>
2023-06-28 23:37:40 -0300

    Add model

---
+++ youtube-cwc-uikit/ConstantsTemplate.swift
@@ -0,0 +1,16 @@
+//
+//  ConstantsTemplate.swift
+//  youtube-cwc-uikit
+//
+//  Created by Leonardo de Oliveira on 28/06/23.
+//
+
+import Foundation
+
+//struct Constants {
+//
+//    static var apiKey = ""
+//    static var playlistId = ""
+//
+//    static var apiUrl = ""
+//}
---
+++ youtube-cwc-uikit/Video.swift
@@ -0,0 +1,18 @@
+//
+//  Video.swift
+//  youtube-cwc-uikit
+//
+//  Created by Leonardo de Oliveira on 28/06/23.
+//
+
+import Foundation
+
+struct Video {
+    
+    var videoId: String = ""
+    var title: String = ""
+    var description: String = ""
+    var thumbUrl: String = ""
+    var publishedAt: String = ""
+    
+    }
