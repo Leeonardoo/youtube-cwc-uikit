2cc459a7220d75992d530412ecd405b5bcb16a36
Leonardo de Oliveira <leeonardo.deo@gmail.com>
2023-07-03 21:12:28 -0300

    Add video model

--- youtube-cwc-uikit/Video.swift
+++ youtube-cwc-uikit/Video.swift
@@ -7,12 +7,42 @@
 
 import Foundation
 
-struct Video {
+struct Video: Decodable {
     
     var videoId: String = ""
     var title: String = ""
     var description: String = ""
     var thumbUrl: String = ""
     var publishedAt: String = ""
-    
+
+    enum CodingKeys: String, CodingKey {
+        
+        case snippet
+        case thumbnails
+        case high
+        case resourceId
+        
+        case videoId
+        case title
+        case description
+        case thumbUrl = "url"
+        case publishedAt
     }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let snippetContainer = try container.nestedContainer(keyedBy: CodingKeys.self, forKey: .snippet)
+        
+        self.title = try snippetContainer.decode(String.self, forKey: .title)
+        self.description = try container.decode(String.self, forKey: .description)
+        self.publishedAt = try container.decode(String.self, forKey: .publishedAt)
+        
+        let thumbnailContainer = try snippetContainer.nestedContainer(keyedBy: CodingKeys.self, forKey: .thumbnails)
+        let highContainer = try thumbnailContainer.nestedContainer(keyedBy: CodingKeys.self, forKey: .high)
+        
+        self.thumbUrl = try highContainer.decode(String.self, forKey: .thumbUrl)
+        
+        let resourceIdContainer = try snippetContainer.nestedContainer(keyedBy: CodingKeys.self, forKey: .resourceId)
+        self.videoId = try resourceIdContainer.decode(String.self, forKey: .videoId)
+    }
+}
